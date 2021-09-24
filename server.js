29ed48f4e88b9fb729a8c8ae9b97cd228c7bb06c
Steve Siegel <stevegabrielsiegel@gmail.com>
2021-09-24 13:34:43 -0500

    mongodb connection work

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const PORT = 3000;
 
 const app = express();
 
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://stevesiegel:S51615161@cluster0.4kvrn.mongodb.net/budget?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 app.use(logger("dev"));
 
 app.use(compression());
@@ -15,10 +24,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
@@ -26,3 +35,6 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+
+
